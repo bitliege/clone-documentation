da109929b6f83366de5e262fe04cbbcf824cb647
joshdrink <git@joshbeveridge.ca>
2019-05-28 17:03:44 -0400

    Adds search, input documentation, and new, not final, colour scheme.

--- app/js/app.js
+++ app/js/app.js
@@ -29,6 +29,93 @@
             lazyLoad: "progressive"
         });
 
+        // Menu Search =========================================================
+        $("#cloneSearch").keyup(function(e) {
+
+            var input = this.value.toLowerCase().trim()
+            var terms = input.split(" ");
+
+            $(".clone-search-results >div >ul >li").each(function(e) {
+
+                var searchText  =  $(this).find("a").data("terms").toLowerCase();
+
+                var show = true;
+
+                terms.forEach(function(term){
+                    if ( !(searchText.indexOf(term.trim()) >= 0) ) {
+                        show = false;
+                    }
+                });
+
+                if (show) {
+                    $(this).show();
+                    $(this).addClass("show");
+                }
+                else {
+                    $(this).hide();
+                    $(this).removeClass("show");
+                }
+
+                if($(".clone-search-results .show").length) {
+                    $(".clone-search-null").hide();
+                }
+                else {
+                    $(".clone-search-null").show();
+                }
+
+            });
+
+            if(terms == "") {
+                $(".clone-search-results").removeClass("active");
+                $(".clone-menu").removeClass("hidden");
+            }
+            else {
+                $(".clone-search-results").addClass("active");
+                $(".clone-menu").addClass("hidden");
+            }
+
+            // Prevents the user from submitting the form with the enter key.
+            $('#cloneSearch').on('keyup keypress', function(e) {
+                var keyCode = e.keyCode || e.which;
+                if (keyCode === 13) {
+
+                    // if ($(".site-grid__sidebar").hasClass("active")) {
+                    //     $(".sidebar__trigger").removeClass("active");
+                    //     $(".site-grid__sidebar-overlay").removeClass("active");
+                    //     $(".site-grid__sidebar").removeClass("active");
+                    // }
+
+                    // var top = $('body').position().top;
+                    // $('html,body').scrollTop(top);
+
+                    e.preventDefault();
+
+                    document.activeElement.blur();
+
+                    return false;
+
+                }
+            });
+
+            e.preventDefault();
+
+        });
+
+        // Menu Triggers =======================================================
+        function menuTrigger(trigger) {
+            if ($(trigger).hasClass("active")) {
+                $(trigger).removeClass("active");
+                $(trigger).next().removeClass("active");
+            } else {
+                $(trigger).addClass("active");
+                $(trigger).next().addClass("active");
+            }
+          }
+    
+          $(document).on("click", "[data-docs-menu-trigger]", function(e) {
+            menuTrigger(this);
+          });
+
     });
 
 })(jQuery);
